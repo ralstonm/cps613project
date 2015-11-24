98794382c5704d74fb4cfe6e4b714b3f46267abc
dbarhoma <daniel.barhoma@ryerson.ca>
2015-11-24 13:52:02 -0500

    Update Game2.vb

--- Game2.vb
+++ Game2.vb
@@ -7,15 +7,24 @@ Public Class Game2
 
     'temp change
     Dim end2 As Integer = 0
-    
+    Dim score As Integer = 0
+
+
 
     Public Sub checkKey(keyCode As Object)
         If keyCode = keyToFind Then
             SAPI.Speak("That's the correct key!")
-            InitializeGame()
+            score = score + 1
+
+            If (score = 7) Then
+                SAPI.Speak("Congratulations! You are a keyboard master")
+                parentFormRef.closeGame2()
+            Else
+                InitializeGame()
+            End If
 
         Else
-            SAPI.Speak("That's not the correct key!")
+                SAPI.Speak("That's not the correct key!")
         End If
 
     End Sub
@@ -57,7 +66,7 @@ Public Class Game2
             parentFormRef.closeGame2()
         End If
         Randomize()
-        counter = CInt(Int((4 * Rnd()) + 1))
+        counter = CInt(Int((15 * Rnd()) + 1))
 
         If (counter = 1) And (Not end2 = 3) Then
             LetterBox.Image = My.Resources.ablock
@@ -87,8 +96,86 @@ Public Class Game2
             keyToFind = Keys.F
             end2 = end2 + 1
         End If
+        If (counter = 5) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.hblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter H on the keyboard?")
+            keyToFind = Keys.H
+            end2 = end2 + 1
+        End If
+        If (counter = 6) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.hblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter I on the keyboard?")
+            keyToFind = Keys.I
+            end2 = end2 + 1
+        End If
+        If (counter = 7) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.jblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter J on the keyboard?")
+            keyToFind = Keys.J
+            end2 = end2 + 1
+        End If
+        If (counter = 8) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.kblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter k on the keyboard?")
+            keyToFind = Keys.K
+            end2 = end2 + 1
+        End If
+        If (counter = 9) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.lblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter L on the keyboard?")
+            keyToFind = Keys.L
+            end2 = end2 + 1
+        End If
+        If (counter = 10) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.mblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter M on the keyboard?")
+            keyToFind = Keys.M
+            end2 = end2 + 1
+        End If
+        If (counter = 11) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.nblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter N on the keyboard?")
+            keyToFind = Keys.N
+            end2 = end2 + 1
+        End If
+        If (counter = 12) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.oblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter O on the keyboard?")
+            keyToFind = Keys.O
+            end2 = end2 + 1
+        End If
+        If (counter = 13) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.pblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter P on the keyboard?")
+            keyToFind = Keys.P
+            end2 = end2 + 1
+        End If
+        If (counter = 14) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.qblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter Q on the keyboard?")
+            keyToFind = Keys.Q
+            end2 = end2 + 1
+        End If
+        If (counter = 15) And (Not end2 = 3) Then
+            LetterBox.Image = My.Resources.rblock
+            LetterBox.SizeMode = PictureBoxSizeMode.StretchImage
+            SAPI.Speak("Can you find the letter R on the keyboard?")
+            keyToFind = Keys.R
+            end2 = end2 + 1
+        End If
 
-      
+
+
 
     End Sub
 
